a1dad07490d32ba772f78e767197fb20bf2fe0b6
Maxence Lange <maxence@artificial-owl.com>
2021-04-26 11:51:38 -0100

    cleaning

--- lib/ActivityPub/Nextcloud/nc21/NC21Signature.php
+++ lib/ActivityPub/Nextcloud/nc21/NC21Signature.php
@@ -291,7 +291,6 @@ class NC21Signature {
 			 ->s('digest', $signedRequest->getDigest())
 			 ->s('host', $request->getHost());
 
-		echo json_encode($data, JSON_PRETTY_PRINT);
 		$signedRequest->setSignatureHeader($data);
 	}
 
