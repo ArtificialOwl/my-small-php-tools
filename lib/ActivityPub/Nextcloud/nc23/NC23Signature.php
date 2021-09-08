1c8af82344b51b3f03dd1e894a6e91b0af0b0a88
Maxence Lange <maxence@artificial-owl.com>
2021-09-08 10:49:49 -0100

    signature

---
+++ lib/ActivityPub/Nextcloud/nc23/NC23Signature.php
@@ -0,0 +1,400 @@
+<?php
+
+declare(strict_types=1);
+
+
+/**
+ * Some tools for myself.
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Maxence Lange <maxence@artificial-owl.com>
+ * @copyright 2021, Maxence Lange <maxence@artificial-owl.com>
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+namespace ArtificialOwl\MySmallPhpTools\ActivityPub\Nextcloud\nc23;
+
+
+use ArtificialOwl\MySmallPhpTools\Exceptions\InvalidOriginException;
+use ArtificialOwl\MySmallPhpTools\Exceptions\ItemNotFoundException;
+use ArtificialOwl\MySmallPhpTools\Exceptions\MalformedArrayException;
+use ArtificialOwl\MySmallPhpTools\Exceptions\SignatoryException;
+use ArtificialOwl\MySmallPhpTools\Exceptions\SignatureException;
+use ArtificialOwl\MySmallPhpTools\Model\Nextcloud\nc23\NC23Request;
+use ArtificialOwl\MySmallPhpTools\Model\Nextcloud\nc23\NC23Signatory;
+use ArtificialOwl\MySmallPhpTools\Model\Nextcloud\nc23\NC23SignedRequest;
+use ArtificialOwl\MySmallPhpTools\Model\SimpleDataStore;
+use ArtificialOwl\MySmallPhpTools\Traits\Nextcloud\nc23\TNC23Signatory;
+use DateTime;
+use Exception;
+use OC;
+use OCP\IRequest;
+
+
+/**
+ * Class NC23Signature
+ *
+ * @package ArtificialOwl\MySmallPhpTools\ActivityPub\Nextcloud\nc23
+ */
+class NC23Signature {
+
+
+	const DATE_HEADER = 'D, d M Y H:i:s T';
+	const DATE_OBJECT = 'Y-m-d\TH:i:s\Z';
+
+	const DATE_TTL = 300;
+
+
+	use TNC23Signatory;
+
+
+	/** @var int */
+	private $ttl = self::DATE_TTL;
+	private $dateHeader = self::DATE_HEADER;
+
+
+	/**
+	 * @param string $body
+	 *
+	 * @return NC23SignedRequest
+	 * @throws InvalidOriginException
+	 * @throws MalformedArrayException
+	 * @throws SignatoryException
+	 * @throws SignatureException
+	 */
+	public function incomingSignedRequest(string $body = ''): NC23SignedRequest {
+		if ($body === '') {
+			$body = file_get_contents('php://input');
+		}
+
+		$this->debug('[<<] incoming', ['body' => $body]);
+
+		$signedRequest = new NC23SignedRequest($body);
+		$signedRequest->setIncomingRequest(OC::$server->get(IRequest::class));
+
+		$this->verifyIncomingRequestTime($signedRequest);
+		$this->verifyIncomingRequestContent($signedRequest);
+		$this->setIncomingSignatureHeader($signedRequest);
+		$this->setIncomingClearSignature($signedRequest);
+		$this->parseIncomingSignatureHeader($signedRequest);
+		$this->verifyIncomingRequestSignature($signedRequest);
+
+		return $signedRequest;
+	}
+
+
+	/**
+	 * @param NC23Request $request
+	 * @param NC23Signatory $signatory
+	 *
+	 * @return NC23SignedRequest
+	 * @throws SignatoryException
+	 */
+	public function signOutgoingRequest(NC23Request $request, NC23Signatory $signatory): NC23SignedRequest {
+		$signedRequest = new NC23SignedRequest($request->getDataBody());
+		$signedRequest->setOutgoingRequest($request)
+					  ->setSignatory($signatory);
+
+		$this->setOutgoingSignatureHeader($signedRequest);
+		$this->setOutgoingClearSignature($signedRequest);
+		$this->setOutgoingSignedSignature($signedRequest);
+		$this->signingOutgoingRequest($signedRequest);
+
+		return $signedRequest;
+	}
+
+
+	/**
+	 * @param NC23SignedRequest $signedRequest
+	 *
+	 * @throws SignatureException
+	 */
+	private function verifyIncomingRequestTime(NC23SignedRequest $signedRequest): void {
+		$request = $signedRequest->getIncomingRequest();
+
+		try {
+			$dTime = new DateTime($request->getHeader('date'));
+			$signedRequest->setTime($dTime->getTimestamp());
+		} catch (Exception $e) {
+			$this->e($e, ['header' => $request->getHeader('date')]);
+			throw new SignatureException('datetime exception');
+		}
+
+		if ($signedRequest->getTime() < (time() - $this->ttl)) {
+			throw new SignatureException('object is too old');
+		}
+	}
+
+
+	/**
+	 * @param NC23SignedRequest $signedRequest
+	 *
+	 * @throws SignatureException
+	 */
+	private function verifyIncomingRequestContent(NC23SignedRequest $signedRequest): void {
+		$request = $signedRequest->getIncomingRequest();
+
+		if (strlen($signedRequest->getBody()) !== (int)$request->getHeader('content-length')) {
+			throw new SignatureException('issue with content-length');
+		}
+
+		if ($request->getHeader('digest') !== ''
+			&& $signedRequest->getDigest() !== $request->getHeader('digest')) {
+			throw new SignatureException('issue with digest');
+		}
+	}
+
+	/**
+	 * @param NC23SignedRequest $signedRequest
+	 */
+	private function setIncomingSignatureHeader(NC23SignedRequest $signedRequest): void {
+		$sign = [];
+		$request = $signedRequest->getIncomingRequest();
+		foreach (explode(',', $request->getHeader('Signature')) as $entry) {
+			if ($entry === '' || !strpos($entry, '=')) {
+				continue;
+			}
+
+			list($k, $v) = explode('=', $entry, 2);
+			preg_match('/"([^"]+)"/', $v, $varr);
+			if ($varr[0] !== null) {
+				$v = trim($varr[0], '"');
+			}
+			$sign[$k] = $v;
+		}
+
+		$signedRequest->setSignatureHeader(new SimpleDataStore($sign));
+	}
+
+
+	/**
+	 * @param NC23SignedRequest $signedRequest
+	 *
+	 * @throws SignatureException
+	 */
+	private function setIncomingClearSignature(NC23SignedRequest $signedRequest): void {
+		$request = $signedRequest->getIncomingRequest();
+		$headers = explode(' ', $signedRequest->getSignatureHeader()->g('headers'));
+
+		$enforceHeaders = array_merge(
+			['content-length', 'date', 'host'],
+			$this->setupArray('enforceSignatureHeaders')
+		);
+		if (!empty(array_diff($enforceHeaders, $headers))) {
+			throw new SignatureException('missing elements in \'headers\'');
+		}
+
+		$target = strtolower($request->getMethod()) . " " . $request->getRequestUri();
+		$estimated = ['(request-target): ' . $target];
+
+		foreach ($headers as $key) {
+			$value = $request->getHeader($key);
+			if (strtolower($key) === 'host') {
+				$value = $signedRequest->getIncomingRequest()->getServerHost();
+			}
+			if ($value === '') {
+				throw new SignatureException('empty elements in \'headers\'');
+			}
+
+			$estimated[] = $key . ': ' . $value;
+		}
+		$signedRequest->setClearSignature(implode("\n", $estimated));
+	}
+
+
+	/**
+	 * @param NC23SignedRequest $signedRequest
+	 *
+	 * @throws MalformedArrayException
+	 * @throws InvalidOriginException
+	 */
+	private function parseIncomingSignatureHeader(NC23SignedRequest $signedRequest): void {
+		$data = $signedRequest->getSignatureHeader();
+		$data->hasKeys(['keyId', 'headers', 'signature'], true);
+
+		$signedRequest->setOrigin($this->getKeyOrigin($data->g('keyId')));
+		$signedRequest->setSignedSignature($data->g('signature'));
+	}
+
+
+	/**
+	 * @param NC23SignedRequest $signedRequest
+	 *
+	 * @throws SignatoryException
+	 * @throws SignatureException
+	 */
+	private function verifyIncomingRequestSignature(NC23SignedRequest $signedRequest) {
+		$data = $signedRequest->getSignatureHeader();
+
+		try {
+			$signedRequest->setSignatory($this->retrieveSignatory($data->g('keyId'), false));
+			$this->verifySignedRequest($signedRequest);
+		} catch (SignatoryException $e) {
+			$signedRequest->setSignatory($this->retrieveSignatory($data->g('keyId'), true));
+			$this->verifySignedRequest($signedRequest);
+		}
+	}
+
+
+	/**
+	 * @param NC23SignedRequest $signedRequest
+	 *
+	 * @throws SignatureException
+	 */
+	private function verifySignedRequest(NC23SignedRequest $signedRequest) {
+		$publicKey = $signedRequest->getSignatory()->getPublicKey();
+		if ($publicKey === '') {
+			throw new SignatureException('empty public key');
+		}
+
+		try {
+			$this->verifyString(
+				$signedRequest->getClearSignature(),
+				base64_decode($signedRequest->getSignedSignature()),
+				$publicKey,
+				$this->getUsedEncryption($signedRequest)
+			);
+		} catch (SignatureException $e) {
+			$this->debug('signature issue', ['signed' => $signedRequest]);
+			throw $e;
+		}
+	}
+
+
+	/**
+	 * @param NC23SignedRequest $signedRequest
+	 */
+	private function setOutgoingSignatureHeader(NC23SignedRequest $signedRequest): void {
+		$request = $signedRequest->getOutgoingRequest();
+
+		$data = new SimpleDataStore();
+		$data->s('(request-target)', NC23Request::method($request->getType()) . ' ' . $request->getPath())
+			 ->sInt('content-length', strlen($signedRequest->getBody()))
+			 ->s('date', gmdate($this->dateHeader))
+			 ->s('digest', $signedRequest->getDigest())
+			 ->s('host', $request->getHost());
+
+		$signedRequest->setSignatureHeader($data);
+	}
+
+
+	/**
+	 * @param NC23SignedRequest $signedRequest
+	 */
+	private function setOutgoingClearSignature(NC23SignedRequest $signedRequest): void {
+		$signing = [];
+		$data = $signedRequest->getSignatureHeader();
+		foreach ($data->keys() as $element) {
+			try {
+				$value = $data->gItem($element);
+				$signing[] = $element . ': ' . $value;
+				if ($element !== '(request-target)') {
+					$signedRequest->getOutgoingRequest()->addHeader($element, $value);
+				}
+			} catch (ItemNotFoundException $e) {
+			}
+		}
+
+		$signedRequest->setClearSignature(implode("\n", $signing));
+	}
+
+
+	/**
+	 * @param NC23SignedRequest $signedRequest
+	 *
+	 * @throws SignatoryException
+	 */
+	private function setOutgoingSignedSignature(NC23SignedRequest $signedRequest): void {
+		$clear = $signedRequest->getClearSignature();
+		$signed = $this->signString($clear, $signedRequest->getSignatory());
+		$signedRequest->setSignedSignature($signed);
+	}
+
+
+	/**
+	 * @param NC23SignedRequest $signedRequest
+	 *
+	 * @return void
+	 */
+	private function signingOutgoingRequest(NC23SignedRequest $signedRequest): void {
+		$headers = array_diff($signedRequest->getSignatureHeader()->keys(), ['(request-target)']);
+		$signatory = $signedRequest->getSignatory();
+		$signatureElements = [
+			'keyId="' . $signatory->getKeyId() . '"',
+			'algorithm="' . $this->getChosenEncryption($signatory) . '"',
+			'headers="' . implode(' ', $headers) . '"',
+			'signature="' . $signedRequest->getSignedSignature() . '"'
+		];
+
+		$signedRequest->getOutgoingRequest()->addHeader('Signature', implode(',', $signatureElements));
+	}
+
+
+	/**
+	 * @param NC23SignedRequest $signedRequest
+	 *
+	 * @return string
+	 */
+	private function getUsedEncryption(NC23SignedRequest $signedRequest): string {
+		switch ($signedRequest->getSignatureHeader()->g('algorithm')) {
+			case 'rsa-sha512':
+				return NC23Signatory::SHA512;
+
+			case 'rsa-sha256':
+			default:
+				return NC23Signatory::SHA256;
+		}
+	}
+
+	/**
+	 * @param NC23Signatory $signatory
+	 *
+	 * @return string
+	 */
+	private function getChosenEncryption(NC23Signatory $signatory): string {
+		switch ($signatory->getAlgorithm()) {
+			case NC23Signatory::SHA512:
+				return 'ras-sha512';
+
+			case NC23Signatory::SHA256:
+			default:
+				return 'ras-sha256';
+		}
+	}
+
+
+	/**
+	 * @param NC23Signatory $signatory
+	 *
+	 * @return int
+	 */
+	public function getOpenSSLAlgo(NC23Signatory $signatory): int {
+		switch ($signatory->getAlgorithm()) {
+			case NC23Signatory::SHA512:
+				return OPENSSL_ALGO_SHA512;
+
+			case NC23Signatory::SHA256:
+			default:
+				return OPENSSL_ALGO_SHA256;
+		}
+	}
+
+}
+
---
+++ lib/Traits/Nextcloud/nc23/TNC23Signatory.php
@@ -0,0 +1,219 @@
+<?php
+
+declare(strict_types=1);
+
+
+/**
+ * Some tools for myself.
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Maxence Lange <maxence@artificial-owl.com>
+ * @copyright 2020, Maxence Lange <maxence@artificial-owl.com>
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+namespace ArtificialOwl\MySmallPhpTools\Traits\Nextcloud\nc23;
+
+
+use ArtificialOwl\MySmallPhpTools\Exceptions\InvalidOriginException;
+use ArtificialOwl\MySmallPhpTools\Exceptions\RequestNetworkException;
+use ArtificialOwl\MySmallPhpTools\Exceptions\SignatoryException;
+use ArtificialOwl\MySmallPhpTools\Exceptions\SignatureException;
+use ArtificialOwl\MySmallPhpTools\Model\Nextcloud\nc23\NC23Request;
+use ArtificialOwl\MySmallPhpTools\Model\Nextcloud\nc23\NC23Signatory;
+
+
+/**
+ * Trait TNC23Signatory
+ *
+ * @package ArtificialOwl\MySmallPhpTools\Traits\Nextcloud\nc23
+ */
+trait TNC23Signatory {
+
+
+	use TNC23Request;
+
+
+	/**
+	 * return Signatory by its Id from cache or from direct request.
+	 * Should be overwritten.
+	 *
+	 * @param string $keyId
+	 * @param bool $refresh
+	 *
+	 * @return NC23Signatory
+	 * @throws SignatoryException
+	 */
+	public function retrieveSignatory(string $keyId, bool $refresh = false): NC23Signatory {
+		if (!$refresh) {
+			throw new SignatoryException();
+		}
+
+		$signatory = new NC23Signatory($keyId);
+		$this->downloadSignatory($signatory, $keyId);
+
+		return $signatory;
+	}
+
+
+	/**
+	 * @param NC23Signatory $signatory
+	 * @param string $keyId
+	 * @param array $params
+	 * @param NC23Request|null $request
+	 *
+	 * @throws SignatoryException
+	 */
+	public function downloadSignatory(
+		NC23Signatory $signatory,
+		string $keyId = '',
+		array $params = [],
+		?NC23Request $request = null
+	): void {
+
+		if (is_null($request)) {
+			$request = new NC23Request();
+			$request->setFollowLocation(true);
+			$request->setTimeout(5);
+		}
+
+		$request->basedOnUrl(($keyId !== '') ? $keyId : $signatory->getId());
+		$request->setParams($params);
+		$request->addHeader('Accept', 'application/ld+json');
+
+		try {
+			$this->updateSignatory($signatory, $this->retrieveJson($request), $keyId);
+		} catch (RequestNetworkException $e) {
+			$this->debug('network issue while downloading Signatory', ['request' => $request]);
+			throw new SignatoryException('network issue: ' . $e->getMessage());
+		}
+	}
+
+
+	/**
+	 * @param NC23Signatory $signatory
+	 * @param array $json
+	 * @param string $keyId
+	 *
+	 * @throws SignatoryException
+	 */
+	public function updateSignatory(NC23Signatory $signatory, array $json, string $keyId = ''): void {
+		$signatory->setOrigData($json)
+				  ->import($json);
+
+		if ($keyId === '') {
+			$keyId = $signatory->getKeyId();
+		}
+
+		try {
+			if (($signatory->getId() !== $keyId && $signatory->getKeyId() !== $keyId)
+				|| $signatory->getId() !== $signatory->getKeyOwner()
+				|| $this->getKeyOrigin($signatory->getKeyId()) !== $this->getKeyOrigin($signatory->getId())
+				|| $signatory->getPublicKey() === '') {
+				$this->debug('invalid format', ['signatory' => $signatory, 'keyId' => $keyId]);
+				throw new SignatoryException('invalid format');
+			}
+		} catch (InvalidOriginException $e) {
+			throw new SignatoryException('invalid origin');
+		}
+	}
+
+
+	/**
+	 * @param string $keyId
+	 *
+	 * @return string
+	 * @throws InvalidOriginException
+	 */
+	public function getKeyOrigin(string $keyId) {
+		$host = parse_url($keyId, PHP_URL_HOST);
+		if (is_string($host) && ($host !== '')) {
+			return $host;
+		}
+
+		throw new InvalidOriginException('cannot retrieve origin from ' . $keyId);
+	}
+
+
+	/**
+	 * @param NC23Signatory $signatory
+	 * @param string $digest
+	 * @param int $bits
+	 * @param int $type
+	 */
+	public function generateKeys(
+		NC23Signatory $signatory,
+		string $digest = 'rsa',
+		int $bits = 2048,
+		int $type = OPENSSL_KEYTYPE_RSA
+	) {
+		$res = openssl_pkey_new(
+			[
+				'digest_alg'       => $digest,
+				'private_key_bits' => $bits,
+				'private_key_type' => $type,
+			]
+		);
+
+		openssl_pkey_export($res, $privateKey);
+		$publicKey = openssl_pkey_get_details($res)['key'];
+
+		$signatory->setPublicKey($publicKey);
+		$signatory->setPrivateKey($privateKey);
+	}
+
+
+	/**
+	 * @param string $clear
+	 * @param NC23Signatory $signatory
+	 *
+	 * @return string
+	 * @throws SignatoryException
+	 */
+	public function signString(string $clear, NC23Signatory $signatory): string {
+		$privateKey = $signatory->getPrivateKey();
+		if ($privateKey === '') {
+			throw new SignatoryException('empty private key');
+		}
+
+		openssl_sign($clear, $signed, $privateKey, $this->getOpenSSLAlgo($signatory));
+
+		return base64_encode($signed);
+	}
+
+
+	/**
+	 * @param string $clear
+	 * @param string $signed
+	 * @param string $publicKey
+	 * @param string $algo
+	 *
+	 * @throws SignatureException
+	 */
+	public function verifyString(
+		string $clear, string $signed, string $publicKey, string $algo = NC23Signatory::SHA256
+	) {
+		if (openssl_verify($clear, $signed, $publicKey, $algo) !== 1) {
+			throw new SignatureException('signature issue');
+		}
+	}
+
+}
+
